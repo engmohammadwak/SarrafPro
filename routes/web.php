dda9d60281e6b6cca6c1d915082f048aa120261a
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-07-25 15:30:01 +0400

    fix: register superadmin routes and middleware in bootstrap/app.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Logout route (used in superadmin layout)
+Route::post('/logout', [\App\Http\Controllers\SuperAdmin\AuthController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
