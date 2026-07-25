2ac01d32f243d6277a996505aa3e4456fc6328ac
Mohammad <engmohammadwk@gmailcom>
2026-07-25 15:49:07 +0400

    Project Update - 25/07/2026 - 03:49 PM

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('super-admin')->middleware(['web', 'super_admin'])->group(function () {
+    Route::resource('shops', ShopController::class);
+    Route::patch('shops/{shop}/suspend',  [ShopController::class, 'suspend'])->name('superadmin.shops.suspend');
+    Route::patch('shops/{shop}/activate', [ShopController::class, 'activate'])->name('superadmin.shops.activate');
 });
