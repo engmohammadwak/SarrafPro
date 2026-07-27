4855740f6fc264398066b5ceddece55ab2471ce6
Mohammad Waleed <38137510+engmohammadwak@users.noreply.github.com>
2026-07-27 17:45:00 +0400

    feat: register PreventBackHistory in web middleware group for Laravel 12

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // منع المتصفح من تخزين الصفحات
+        $middleware->appendToGroup('web', \App\Http\Middleware\PreventBackHistory::class);
+
         $middleware->alias([
             'admin'       => \App\Http\Middleware\AdminMiddleware::class,
             'super_admin' => \App\Http\Middleware\SuperAdminMiddleware::class,
